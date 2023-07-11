feba1b454d12aee97fc4e840a515f13352605ce9
Pavel Tomáško <ptomask@gmail.com>
2023-07-11 15:28:10 +0200

    FIX: Delete unnecessary file

--- frontend-html/src/plugins/implementations/src/taldis-supplier-invoice/bootstrap.tsx
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from "plugins/tools/PluginLibrary";
-import { TaldisSupplierInvoicePlugin } from "./taldisSupplierInvoiceBootstrap";
-
-export const bootstrap = () => {
-  registerPlugin("TaldisSupplierInvoicePlugin", () => {
-    return new TaldisSupplierInvoicePlugin();
-  });
-}
